d4fea23a57603b89e16f937e5b61ed7ddda73ba4
sensei <manishkumar97430@gmail.com>
2024-11-10 23:15:54 +0530

    added the basic tokens for tokenizer

--- tokenizer/token.go
+++ tokenizer/token.go
@@ -1,1 +1,39 @@
 package tokenizer
+
+// tokentype represents the type of token
+type TokenType int
+
+const (
+	ObjectStart TokenType = iota
+	ObjectEnd
+	ArrayStart
+	ArrayEnd
+	String
+	Number
+	Null
+)
+
+// token represets single tokn which holds type vand value --
+type Token struct {
+	Type  TokenType
+	Value string
+}
+
+func (t TokenType) String() string {
+	switch t {
+	case ObjectStart:
+		return "ObjectStart"
+	case ObjectEnd:
+		return "ObjectEnd"
+	case ArrayStart:
+		return "ArrayStart"
+	case String:
+		return "String"
+	case Number:
+		return "number"
+	case Null:
+		return "Null"
+	default:
+		return "Unknown"
+	}
+}
