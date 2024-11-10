482201fcc048cce550fc83719ea67760c28f34a4
sensei <manishkumar97430@gmail.com>
2024-11-10 23:09:10 +0530

    setup go project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mnsdojo/go-json-parser
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	jsonInput := `{
+        "name": "Alice",
+        "age": 30,
+        "isStudent": false,
+        "grades": [95, 88, 92],
+        "address": {
+            "city": "Wonderland",
+            "zipcode": null
+        }
+    }`
+}
